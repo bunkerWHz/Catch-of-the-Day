1a5ddefd0671ea48dce057b4ca88607466250e65
aparsegov <aparsegov@gmail.com>
2017-07-03 18:03:44 +0300

    first commit

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCFmtL6BU12gUwUXPowR_heGemEZCIumzY",
+	authDomain: "sushi-order-thing.firebaseapp.com",
+	databaseURL: "https://sushi-order-thing.firebaseio.com",
+});
+
+export default base;
